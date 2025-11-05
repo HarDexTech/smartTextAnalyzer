f085e958c608ea767b78281a586718a84545976c
Adesina Ayomide <adesinayomide2@gmail.com>
2025-11-05 09:37:45 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const findWords = () => {
+    let words = document.getElementById('inputText').value;
+    console.log(words)
+    let wordToFind = prompt(`Enter Word To Find:`);
+    console.log(wordToFind)
+}
+
+const countChar = () => {
+    let words = document.getElementById('inputText').value;
+    console.log(words.length)
+}
+
+const countWord = () => {}
